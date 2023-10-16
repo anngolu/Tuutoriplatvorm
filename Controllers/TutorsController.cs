6d67680dd872610df22dbf092ea219a2f82c5dfa
Anastassia Fedortseva <afedor@ttu.ee>
2023-10-16 12:02:54 +0300

    TutorsController done

--- Controllers/TutorsController.cs
+++ Controllers/TutorsController.cs
@@ -13,16 +13,51 @@ namespace tuutoriplatvorm.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetStudent(
+        public IActionResult GetTutor(
             [FromQuery] Town? town,
             [FromQuery] University? university,
             [FromQuery] Speciality? speciality,
             [FromQuery] Subject? subject){
-                return Ok(_context.StudentList?.Where(s => 
+                return Ok(_context.TutorList?.Where(s => 
                 !town.HasValue || s.Town == town.Value &&
                 !university.HasValue! || s.University == university!.Value &&
                 !speciality.HasValue || s.Speciality == speciality!.Value &&
                 !subject.HasValue || s.Subject == subject!.Value));
             }
+
+            [HttpGet]
+        public IActionResult GetTutor(){
+            return Ok(_context.TutorList);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetDetailById(int? id){
+            var tutors = _context.TutorList?.FirstOrDefault(s => s.Id == id);
+            if(tutors == null){
+                return NotFound();
+            }
+            return Ok(tutors);
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] Tutors tutor){
+            var dbTutor = _context.TutorList?.Find(tutor.Id);
+            if(dbTutor == null){
+                _context.Add(tutor);
+                _context.SaveChanges();
+                return CreatedAtAction(nameof(GetDetailById), new {Id = tutor.Id}, tutor);
+            }
+            else{
+                return Conflict();
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int? id, [FromBody] Tutors tutors){
+            if(id != tutors.Id || !_context.TutorList!.Any(s => s.Id == id)){
+                return NotFound();
+            }
+            _context.Update(tutors);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
