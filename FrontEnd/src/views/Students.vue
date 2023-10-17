685089052a8ea36c2f0b4f7c1757a22d3d97f854
Sofiia Hapon <sohapo@ttu.ee>
2023-10-17 10:12:38 +0300

    erorri parandamine

--- FrontEnd/src/views/Students.vue
+++ FrontEnd/src/views/Students.vue
@@ -1,10 +1,9 @@
 <template>
-    <TutorList :title="title" />
-  </template>
-  
-  <script setup lang="ts">
-  import StudentList from '@/components/StudentList.vue';
-  
-  defineProps<{ title: String }>();
-  </script>
-  
+  <StudentList :title="title" />
+</template>
+
+<script setup lang="ts">
+import StudentList from '@/components/StudentList.vue';
+
+defineProps<{ title: String }>();
+</script>
