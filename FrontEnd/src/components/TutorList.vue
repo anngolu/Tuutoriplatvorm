7ec32c17bd04edf714b487b06d3694fe582dcbd6
Sofiia Hapon <sohapo@ttu.ee>
2023-11-12 19:20:22 +0200

    filtreerimine ülikooli järgi

--- FrontEnd/src/components/TutorList.vue
+++ FrontEnd/src/components/TutorList.vue
@@ -2,26 +2,21 @@
   <div class="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px=8 text-dark-300">
     <div class="text-center">
       <h1 class="font-bold">Tuutorite nimekiri</h1>
-      <input placeholder="Otsi nime järgi" v-model="nameSearch" />
-      <DataTable :value="searchResult">
+      <input placeholder="Otsi nime järgi" v-model="nameSearch"> 
+      <input placeholder="Otsi ülikooli järgi" v-model="universitySearch" />
+      <DataTable :value="filteredTutors">
         <Column field="name" header="Nimi" />
         <Column field="university" header="Ülikool" />
         <Column field="speciality" header="Teaduskond" />
-        <Column header="Aine">
-          <template #body="slotProps">
-            {{ subjectsToStringConvert(slotProps.data.subjects) }}
-          </template>
-        </Column>
+        <Column field="subject" header="Aine" />
+        <Column field="hourlyPrice" header="Tunnihind" sortable style/>
         <Column field="hourlyPrice" header="Tunnihind" sortable style />
-        <Column field="averageRate" header="Reiting" sortable style />
+        <Column field="averageRate" header="Reiting" sortable style/>
         <Column header="Hinda">
           <template #body="slotProps">
-            <Rating
-              v-on:change="submitRate($event, slotProps.data.id)"
-              :cancel="false"
-            />
+              <Rating v-on:change="submitRate($event, slotProps.data.id)" :cancel="false" />
           </template>
-        </Column>
+      </Column>
       </DataTable>
     </div>
   </div>
@@ -31,30 +26,25 @@
 //import { storeToRefs } from 'pinia';
 import { useTutorsStore } from '@/stores/tutorsStore';
 import { RatingChangeEvent } from 'primevue/rating';
-import { onMounted, ref, computed } from 'vue';
-import { Subject } from '@/model/tutor';
+import { onMounted, ref, computed} from 'vue';
+
 
 const tutorsStore = useTutorsStore();
+const nameSearch = ref("");
+const universitySearch = ref("");
 
-//const { tutors } = storeToRefs(tutorsStore);
-const nameSearch = ref('');
 const submitRate = (event: RatingChangeEvent, id: string) => {
-  // console.log(event);
-  tutorsStore.calculateRating(id, event.value).then((_) => tutorsStore.load());
-};
+  tutorsStore.calculateRating(id, event.value)
+    .then(_ => tutorsStore.load());
+}
 
-// /* // watch(nameSearch,(val)=>{
-//   const searchResult=tutorsStore.searchTutorByName(val)
-//   console.log("vaste: ", searchResult)
-// })
-//  */
+const filteredTutors = computed(() => {
+  return tutorsStore.tutors.filter((tutor) => {
+    const nameMatch = tutor.name.toLowerCase().includes(nameSearch.value.toLowerCase());
+    const universityMatch = tutor.university?.toLowerCase().includes(universitySearch.value.toLowerCase());
 
-const subjectsToStringConvert = (subjects: Subject[]) => {
-  return subjects.join(', ');
-};
-
-const searchResult = computed(() => {
-  return tutorsStore.searchTutorByName(nameSearch.value);
+    return nameMatch && universityMatch;
+  });
 });
 
 onMounted(() => {
